1f2a4d52b085aba705bf7593c5f06fc683560c87
michel <michelc10@hotmail.com>
2019-06-16 00:09:58 -0430

    Proyecto frases de breaking bad con react hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Frase({frase}) {
+  return(
+    <div className="frase">
+      <h1>{frase.quote}</h1>
+      <p>- {frase.author}</p>
+    </div>
+  )
+}
 
 function App() {
+
+  const [frase, obtenerFrase] = useState({});
+
+  const consultarAPI = async () => {
+    const resultado = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+
+    //Agregar el resultado de la api al state; (similar a this.set.state)
+    obtenerFrase(resultado.data[0]);
+
+  }
+
+  // consulta a una rest api
+  useEffect(
+    () => {
+      consultarAPI()
+    }, []
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor">
+      <br/>
+      <Frase 
+        frase={frase}
+      />
+      <button
+        onClick={consultarAPI}
+      >Generar nueva</button>
     </div>
-  );
+  )
 }
 
 export default App;
